87186a0d3c5e2c49ab1eac6083eb0b23bd5f457b
VahanAper <asryanvahandev@gmail.com>
2017-05-24 17:08:53 +0400

    replaced by bootstrap components

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import {
+  FormGroup,
+  FormControl,
+  InputGroup,
+  Glyphicon,
+} from 'react-bootstrap';
+
+import './app.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app">
         <div className="app-title">Music Maser</div>
-        <div>
-          <input placeholder="Search an artist ..." />
-          <button>Search</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search an Artist ..."
+            />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search" />
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
         <div className="profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
